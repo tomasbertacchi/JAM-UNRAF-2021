39967d7cb6c8409af75df99b8f18d56f6fe23f33
alva531 <alvaro.jorge531@gmail.com>
2021-09-25 21:42:28 -0300

    robot

--- src/scenes/game.ts
+++ src/scenes/game.ts
@@ -9,7 +9,7 @@ export default class game extends Phaser.Scene
     private cursor_A!: Phaser.Input.Keyboard.Key
     private cursor_D!: Phaser.Input.Keyboard.Key
     private cursor_S!: Phaser.Input.Keyboard.Key
-    private cursor_CTRL!: Phaser.Input.Keyboard.Key
+    private cursor_SHIFT!: Phaser.Input.Keyboard.Key
     private debeMoverse!: Boolean
     private estaAtacando!: Boolean
     private seDefiende!: Boolean
@@ -39,18 +39,18 @@ export default class game extends Phaser.Scene
         this.cursor_A = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.A);
         this.cursor_D = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.D);
         this.cursor_S = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.S);
-        this.cursor_CTRL = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.CTRL);
+        this.cursor_SHIFT = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.SHIFT);
         //plataformas
         this.plataformas = this.physics.add.staticGroup();
         this.plataformas.create(960, 1060, 'plataforma') 
 
 
         //personajes
-        this.personaje = this.physics.add.sprite(200, 950, "personaje3", 0).setScale(5);
-        this.personaje2 = this.physics.add.sprite(1700, 950, "personaje3", 0).setScale(5)
+        this.personaje = this.physics.add.sprite(200, 500, "personajeparado", 0);
+        this.personaje2 = this.physics.add.sprite(1700, 500, "personajeparado", 0);
         this.personaje2.flipX = true;
-        this.personaje.setSize(15,35)
-        this.personaje2.setSize(15,35)
+        this.personaje.setSize(320,587)
+        this.personaje2.setSize(320,587)
         this.physics.add.collider(this.personaje, this.plataformas);
         this.physics.add.collider(this.personaje2, this.plataformas);
         this.physics.add.collider(this.personaje, this.personaje2, ()=>{this.ataque()})
@@ -67,15 +67,16 @@ export default class game extends Phaser.Scene
         //RIGHT
         if (this.cursor_D.isDown && this.debeMoverse == true){
             this.personaje.setVelocityX(300)
-            this.personaje.setSize(15,35)
+            this.personaje.setSize(320,587)
             this.personaje.play("run", true)
         } //LEFT
         else if (this.cursor_A.isDown && this.debeMoverse == true){
             this.personaje.setVelocityX(-300)
-            this.personaje.setSize(15,35)
+            this.personaje.setSize(320,587)
         } //IDLE
         else if(this.debeMoverse == true){
             this.personaje.setVelocityX(0)
+            this.personaje.setSize(320,587)
             this.personaje.play("idle",true)
         }
         
@@ -85,9 +86,9 @@ export default class game extends Phaser.Scene
             this.estaAtacando = true
             this.personaje.setVelocityX(0)
             if(this. estaAtacando == true){
-                this.personaje.setSize(43,35)
+                this.personaje.setSize(330,587)
                 this.personaje.play("ataque1")
-                .on("animationcomplete", () => {this.personaje.play("idle",true); this.debeMoverse = true; this.estaAtacando = false; this.personaje.setSize(15,35)})
+                .on("animationcomplete", () => {this.personaje.play("idle",true); this.debeMoverse = true; this.estaAtacando = false; this.personaje.setSize(320,587)})
                 
             } 
             
@@ -99,7 +100,7 @@ export default class game extends Phaser.Scene
             this.personaje.setVelocityX(0)
             if(this. seDefiende == true){
                 this.personaje.play("escudo")
-                .on("animationcomplete", () => {this.personaje.play("idle",true); this.debeMoverse = true; this.seDefiende = false; this.personaje.setSize(15,35)})   
+                .on("animationcomplete", () => {this.personaje.play("idle",true); this.debeMoverse = true; this.seDefiende = false; this.personaje.setSize(320,587)})   
             } 
 
         }
@@ -108,26 +109,27 @@ export default class game extends Phaser.Scene
         //RIGHT
        if (this.cursores.right.isDown && this.debeMoverse2 == true){
             this.personaje2.setVelocityX(300)
-            this.personaje2.setSize(15,35)
+            this.personaje2.setSize(320,587)
        }//LEFT
        else if (this.cursores.left.isDown && this.debeMoverse2 == true){
            this.personaje2.setVelocityX(-300)
-           this.personaje2.setSize(15,35)
+           this.personaje2.setSize(320,587)
            this.personaje2.play("run", true)
        } //IDLE
        else if(this.debeMoverse2 == true){
         this.personaje2.setVelocityX(0)
+        this.personaje2.setSize(320,587)
         this.personaje2.play("idle",true)
     }
         // ATAQUE
-       if(Phaser.Input.Keyboard.JustDown(this.cursor_CTRL) && this.estaAtacando2 == false){
+       if(Phaser.Input.Keyboard.JustDown(this.cursor_SHIFT) && this.estaAtacando2 == false && this.seDefiende2 == false){
             this.debeMoverse2 = false
             this.estaAtacando2 = true
             this.personaje2.setVelocityX(0)
             if(this.estaAtacando2 == true){
-                this.personaje2.setSize(43,35)
+                this.personaje2.setSize(330,587)
                 this.personaje2.play("ataque2")
-                .on("animationcomplete", () => {this.personaje2.play("idle",true); this.debeMoverse2 = true; this.estaAtacando2 = false; this.personaje2.setSize(15,35)})
+                .on("animationcomplete", () => {this.personaje2.play("idle",true); this.debeMoverse2 = true; this.estaAtacando2 = false; this.personaje2.setSize(320,587)})
             } 
        }
 
@@ -137,7 +139,7 @@ export default class game extends Phaser.Scene
         this.personaje2.setVelocityX(0)
         if(this. seDefiende2 == true){
             this.personaje2.play("escudo")
-            .on("animationcomplete", () => {this.personaje2.play("idle",true); this.debeMoverse2 = true; this.seDefiende2 = false; this.personaje2.setSize(15,35)})   
+            .on("animationcomplete", () => {this.personaje2.play("idle",true); this.debeMoverse2 = true; this.seDefiende2 = false; this.personaje2.setSize(320,587)})   
         }
     } 
 
--- src/scenes/preloader.ts
+++ src/scenes/preloader.ts
@@ -10,9 +10,11 @@ export default class preloader extends Phaser.Scene
 	preload()
     {
         this.load.image("plataforma", "assets/escenario/plataforma.png")
-        this.load.spritesheet("personaje", "assets/personaje/personaje.png",{ frameWidth: 50, frameHeight: 37})
-        this.load.spritesheet("personaje2", "assets/personaje/personaje.png",{ frameWidth: 50, frameHeight: 37, endFrame:71  })
-        this.load.spritesheet("personaje3", "assets/personaje/personaje3.png", { frameWidth: 50, frameHeight: 37, endFrame:71 })
+        this.load.spritesheet("personajeparado", "assets/personaje/Fitz_parado.png",{ frameWidth: 381, frameHeight: 587, endFrame:3  })
+        this.load.spritesheet("personajecamina", "assets/personaje/Fitz_camina.png",{ frameWidth: 381, frameHeight: 587, endFrame:3  })
+        this.load.spritesheet("personajeataca", "assets/personaje/Fitz_ataca.png",{ frameWidth: 381, frameHeight: 587, endFrame:3  })
+        this.load.spritesheet("personajedefiende", "assets/personaje/Fitz_Defensa.png", { frameWidth: 381, frameHeight: 587, endFrame:3 })
+        this.load.spritesheet("personajemuere", "assets/personaje/Fitz_Muere.png", { frameWidth: 381, frameHeight: 587, endFrame:3 })
         this.load.image("tuerca", "images/tuerca.png")
     }
 
@@ -23,19 +25,19 @@ export default class preloader extends Phaser.Scene
         //personaje 1
         this.anims.create({
             key: 'idle',
-            frames: this.anims.generateFrameNumbers('personaje3', { start: 0, end: 3 }),
+            frames: this.anims.generateFrameNumbers('personajeparado', { start: 0, end: 3 }),
             repeat: -1,
             frameRate: 5
         });
         this.anims.create({
             key: 'run',
-            frames: this.anims.generateFrameNumbers('personaje3', { start: 8, end: 13 }),
+            frames: this.anims.generateFrameNumbers('personajecamina', { start: 0, end: 3 }),
             repeat: -1,
             frameRate: 5
         });
         this.anims.create({
             key: 'ataque1',
-            frames: this.anims.generateFrameNumbers('personaje3', { start: 49, end: 52 }),
+            frames: this.anims.generateFrameNumbers('personajeataca', { start: 0, end: 3 }),
             repeat: 0,
             frameRate: 10
         });
@@ -43,28 +45,28 @@ export default class preloader extends Phaser.Scene
         //personaje 2
         this.anims.create({
             key: 'ataque2',
-            frames: this.anims.generateFrameNumbers('personaje3', { start: 49, end: 52 }),
+            frames: this.anims.generateFrameNumbers('personajeataca', { start: 0, end: 3 }),
             repeat: 0,
             frameRate: 10,
         });
 
         this.anims.create({
             key: 'muerte1',
-            frames: this.anims.generateFrameNumbers('personaje3', { start: 65, end: 68 }),
+            frames: this.anims.generateFrameNumbers('personajemuere', { start: 0, end: 3 }),
             repeat: 0,
             frameRate: 10,
         });
 
         this.anims.create({
             key: 'muerte2',
-            frames: this.anims.generateFrameNumbers('personaje3', { start: 65, end: 68 }),
+            frames: this.anims.generateFrameNumbers('personajemuere', { start: 0, end: 3 }),
             repeat: 0,
             frameRate: 10,
         });
 
         this.anims.create({
             key: 'escudo',
-            frames: this.anims.generateFrameNumbers('personaje3', { start: 33, end: 34 }),
+            frames: this.anims.generateFrameNumbers('personajedefiende', { start: 0, end: 3 }),
             repeat: 0,
             frameRate: 10,
         });
